7d404b8b182033734e3ebd05ee9dda1cdd95014f
Suyash629 <64470825+Suyash629@users.noreply.github.com>
2021-11-09 12:14:29 +0530

    Day1

---
+++ band.py
@@ -0,0 +1,16 @@
+#WAP to show below flow
+#Band Name Generator
+
+# Hello! Welcome to band name generator
+#Please enter your pet name: som
+
+#Please Enter the city you grew in: jalna
+
+#Your band name is SomJalna
+
+petName = "str1"
+city = "str2"
+phone = 986533
+conc = petName+" "+city+ str(phone)
+
+print(conc)
---
+++ hello.py
@@ -0,0 +1,42 @@
+# programming
+# WAP to show following flow
+
+# Greetings! Please enter your name: <sss>
+# Hello <sss>. Welcome to Library.
+# Press Enter to continue...
+
+# 1. What is Computer?
+# -> 1 line
+
+# Press Enter to continue...
+
+# 2. What is Programming?
+# -> 1 line
+
+
+# Press Enter to continue....
+
+# 3. What is RAM?
+# -> 1 line
+
+# Press Enter to continue...
+
+# 4. What is OS?
+# -> 1 line
+
+# Press Enter to continue...
+
+# Thank you, see you next time
+
+
+
+
+print("HELLO")
+name = input("Enter your name: ")
+print(name)
+input("Press Enter to continue")
+print("After enter")
+
+
+
+
